5a8fd80e5f7ae077e7a702606706386ed7197f14
lustig314 <60037706+lustig314@users.noreply.github.com>
2022-03-22 21:11:34 +0300

    add photo

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
+const langRuArr = {
+  title: 'Портфолио',
+  home: 'Главная',
+  about: 'Обо мне',
+  skills: 'Навыки',
+  portfolio: 'Портфолио',
+  contacts: 'Контакты',
+  mainTitle: 'Портфолио. Игорь Бабин - Junior Front-end разработчик',
+  headerName: 'Игорь Бабин',
+  headerInfo: 'Junior Front-end разработчик 25 лет, Тамбов',
+  aboutText1: 'Привет, я Игорь - Junior Front-end разработчик из Тамбова. Интересуюсь front-end разработкой и всем, что связано с этим.',
+  aboutText2: 'Прошел курсы в HTML Academy по HTML/CSS и JavaScript.',
+  aboutText3: 'Буду рад работать над интересными проектами в дружном коллективе.',
+  skillsText: 'Я знаю такие технологии, как',
+  portfolioText1: 'Одностраничный сайт - Mavic 2 Pro',
+  portfolioText2: 'React поисковый сервис фильмов',
+  portfolioText3: 'Сервис поиска жилья - Keksobooking',
+  contactsText1: 'Хотите узнать больше или просто поболтать? Пишите!',
+  contactsText2: 'Жду вас на GitHub, Telegram, VK, WhatsApp',
+  contactsBtn: 'Написать',
+}
+
 const burger = document.querySelector('.burger-menu')
 const navMenu = document.querySelector('.menu')
 const langSwitcher = document.querySelector('.header__switcher')
